8de50f1ec3c32baa559107655e9f1e1c2b452c24
LOubouzar <LOubouzar@gmail.com>
2018-09-23 15:05:22 -0400

    combined backend code

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,101 +1,109 @@
-require("dotenv").config();
-var imaggaapiKey = process.env.IMAGGA_ID
-var imaggaapiSecret = process.env.IMAGGA_SECRET
+// require("dotenv").config();
 
-// Parses our HTML and helps us find elements
-var cheerio = require("cheerio");
-// Makes HTTP request for HTML page
-var request = require("request");
+// var bodyParser = require("body-parser");
+// var imaggaapiKey = process.env.IMAGGA_ID
+// var imaggaapiSecret = process.env.IMAGGA_SECRET
 
-function scraper(colorsName0) {
-    request("https://www.etsy.com/search/art-and-collectibles?q=" + colorsName0 + "&explicit=1", function (err, res, body) {
+// // Parses our HTML and helps us find elements
+// var cheerio = require("cheerio");
+// // Makes HTTP request for HTML page
+// var request = require("request");
 
-        console.log("Etsy Scraper Variable: " + colorsName0);
-        if (err) console.error(err);
-        var $ = cheerio.load(body);
+// //GET request in server for API 
+// function scraper(colorsName0) {
+//     var results = [];
+//     request("https://www.etsy.com/search/art-and-collectibles?q=" + colorsName0 + "&explicit=1", function (err, res, body) {
 
-        var resultsURL = [];
-        var resultsPrice = [];
-        var resultsName = [];
-        var resultsImage = [];
+//         console.log("Etsy Scraper Variable: " + colorsName0);
+//         if (err) console.error(err);
+//         var $ = cheerio.load(body);
 
-        $(".currency-value").each(function (i, e) {
-            var price = $(e).text();
-            resultsPrice.push(price);
-        });
+//         var resultsURL = [];
+//         var resultsPrice = [];
+//         var resultsName = [];
+//         var resultsImage = [];
+        
 
-        $("p.text-body").each(function (i, e) {
-            var name = $(e).text();
-            resultsName.push(name);
-        });
+//         $(".currency-value").each(function (i, e) {
+//             var price = $(e).text();
+//             resultsPrice.push(price);
+//         });
 
-        $(".width-full.display-block.position-absolute").each(function (i, e) {
-            var image = $(e).attr();
-            resultsImage.push(image);
-        });
+//         $("p.text-body").each(function (i, e) {
+//             var name = $(e).text();
+//             resultsName.push(name);
+//         });
 
-        $(".display-inline-block.listing-link").each(function (i, e) {
-            var url = $(e).attr();
-            resultsURL.push(url);
-        });
+//         $(".width-full.display-block.position-absolute").each(function (i, e) {
+//             var image = $(e).attr();
+//             resultsImage.push(image);
+//         });
 
-        //  results.push({
-        //    price: "$'#price1'",
-        //    image: "$'#img1'",
-        //    name: "$'#name1'",
-        //    url: "$'#url1'",
-        //  });
+//         $(".display-inline-block.listing-link").each(function (i, e) {
+//             var url = $(e).attr();
+//             resultsURL.push(url);
+//         });
 
-        console.log("name" + resultsName);
-        console.log(resultsImage);
-        console.log(resultsURL);
-        console.log("price" + resultsPrice);
+//          results.push({
+//            price: resultsPrice,
+//            image: resultsImage,
+//            name: resultsName,
+//            url: resultsURL,
+//          });
 
-    });
+//         // console.log("name" + resultsName);
+//         // console.log(resultsImage);
+//         // console.log(resultsURL);
+//         // console.log("price" + resultsPrice);
+//         console.log(results);
 
+//         return results;
 
+//     });
+// };
 
-//URL image color extractor and console logged to the CLI
-var requestImagga = require("request"),
-    imageUploadURL = "https://i.etsystatic.com/15309986/r/il/43c914/1528912866/il_570xN.1528912866_6ht5.jpg";
+// //URL image color extractor and console logged to the CLI
+// function imager() {
+//     var requestImagga = require("request"),
+//         imageUploadURL = "https://i.etsystatic.com/15309986/r/il/43c914/1528912866/il_570xN.1528912866_6ht5.jpg";
 
-requestImagga.get("https://api.imagga.com/v1/colors?url=" + encodeURIComponent(imageUploadURL), function (error, response, body) {
-    var b = JSON.parse(body);
-    //Console log of the General JSON of the image information API
-    // console.log(b.results[0].info.image_colors[0]);
-    var colorsName0 = b.results[0].info.image_colors[0].closest_palette_color;
-
-    console.log("Primary Closest Color Name: ", colorsName0);
-
-    scraper(colorsName0);
-
-}).auth(imaggaapiKey, imaggaapiSecret, true);
-
-};
-
-scraper();
-
-// file uploader to Imagga API  https://docs.imagga.com/#content
-// var fs = require('fs'),
-//     request = require("request"),
-//     filePath = "/path/to/image.jpg",
-//     formData = {
-//         image: fs.createReadStream(filePath)
-//     };
-
-// request.post({ url: 'https://api.imagga.com/v1/content', formData: formData },
-//     function (error, response, body) {
+//     requestImagga.get("https://api.imagga.com/v1/colors?url=" + encodeURIComponent(imageUploadURL), function (error, response, body) {
 //         var b = JSON.parse(body);
+//         //Console log of the General JSON of the image information API
+//         // console.log(b.results[0].info.image_colors[0]);
 //         var colorsName0 = b.results[0].info.image_colors[0].closest_palette_color;
-//         var colorsHTML0 = b.results[0].info.image_colors[0].closest_palette_color_html_code;
-//         var colorsPercent0 = b.results[0].info.image_colors[0].percent;
-//         var colorsName1 = b.results[0].info.image_colors[1].closest_palette_color;
-//         var colorsHTML1 = b.results[0].info.image_colors[1].closest_palette_color_html_code;
-//         var colorsPercent1 = b.results[0].info.image_colors[1].percent;
+
 //         console.log("Primary Closest Color Name: ", colorsName0);
-//         console.log("Primary HTML Color Code: ", colorsHTML0);
-//         console.log("Primary Color Percentage: ", colorsPercent0, "%");
-//         console.log("Secondary Closest Color Name: ", colorsName1);
-//         console.log("Secondary HTML Color Code: ", colorsHTML1);
-//         console.log("Secondary Color Percentage: ", colorsPercent1, "%");
+
+//         scraper(colorsName0);
+
 //     }).auth(imaggaapiKey, imaggaapiSecret, true);
+// };
+
+// imager();
+
+
+// // file uploader to Imagga API  https://docs.imagga.com/#content
+// // var fs = require('fs'),
+// //     request = require("request"),
+// //     filePath = "/path/to/image.jpg",
+// //     formData = {
+// //         image: fs.createReadStream(filePath)
+// //     };
+
+// // request.post({ url: 'https://api.imagga.com/v1/content', formData: formData },
+// //     function (error, response, body) {
+// //         var b = JSON.parse(body);
+// //         var colorsName0 = b.results[0].info.image_colors[0].closest_palette_color;
+// //         var colorsHTML0 = b.results[0].info.image_colors[0].closest_palette_color_html_code;
+// //         var colorsPercent0 = b.results[0].info.image_colors[0].percent;
+// //         var colorsName1 = b.results[0].info.image_colors[1].closest_palette_color;
+// //         var colorsHTML1 = b.results[0].info.image_colors[1].closest_palette_color_html_code;
+// //         var colorsPercent1 = b.results[0].info.image_colors[1].percent;
+// //         console.log("Primary Closest Color Name: ", colorsName0);
+// //         console.log("Primary HTML Color Code: ", colorsHTML0);
+// //         console.log("Primary Color Percentage: ", colorsPercent0, "%");
+// //         console.log("Secondary Closest Color Name: ", colorsName1);
+// //         console.log("Secondary HTML Color Code: ", colorsHTML1);
+// //         console.log("Secondary Color Percentage: ", colorsPercent1, "%");
+// //     }).auth(imaggaapiKey, imaggaapiSecret, true);
--- server.js
+++ server.js
@@ -1,21 +1,139 @@
 // Dependencies
-
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
+// Parses our HTML and helps us find elements
+var cheerio = require("cheerio");
+// Makes HTTP request for HTML page
+var request = require("request");
 
 //Express Configuration
 var app = express();
 
-var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+
+// app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.json());
 
 //Required Routes
-require("./app/routing/api-routes")(app);
-require("./app/routing/html-routes")(app);
+// require("./app/routing/api-routes")(app);
+// require("./app/routing/html-routes")(app);
+
+
+
+imager();
+
+require("dotenv").config();
+
+var imaggaapiKey = process.env.IMAGGA_ID
+var imaggaapiSecret = process.env.IMAGGA_SECRET
+
+//GET request in server for API 
+function scraper(colorsName0) {
+    var results = [];
+    request("https://www.etsy.com/search/art-and-collectibles?q=" + colorsName0 + "&explicit=1", function (err, res, body) {
+
+        console.log("Etsy Scraper Variable: " + colorsName0);
+        if (err) console.error(err);
+        var $ = cheerio.load(body);
+
+        var resultsURL = [];
+        var resultsPrice = [];
+        var resultsName = [];
+        var resultsImage = [];
+        
+
+        $(".currency-value").each(function (i, e) {
+            var price = $(e).text();
+            resultsPrice.push(price);
+        });
+
+        $("p.text-body").each(function (i, e) {
+            var name = $(e).text();
+            resultsName.push(name);
+        });
+
+        $(".width-full.display-block.position-absolute").each(function (i, e) {
+            var image = $(e).attr();
+            resultsImage.push(image);
+        });
+
+        $(".display-inline-block.listing-link").each(function (i, e) {
+            var url = $(e).attr();
+            resultsURL.push(url);
+        });
+
+         results.push({
+           price: resultsPrice,
+           image: resultsImage,
+           name: resultsName,
+           url: resultsURL,
+         });
+
+        // console.log("name" + resultsName);
+        // console.log(resultsImage);
+        // console.log(resultsURL);
+        // console.log("price" + resultsPrice);
+        console.log(results);
+
+        return results;
+
+    });
+};
+
+//URL image color extractor and console logged to the CLI
+function imager() {
+    var requestImagga = require("request"),
+        imageUploadURL = "https://i.etsystatic.com/15309986/r/il/43c914/1528912866/il_570xN.1528912866_6ht5.jpg";
+
+    requestImagga.get("https://api.imagga.com/v1/colors?url=" + encodeURIComponent(imageUploadURL), function (error, response, body) {
+        console.log("Imagga API:" + error)
+        var b = JSON.parse(body);
+        //Console log of the General JSON of the image information API
+        // console.log(b.results[0].info.image_colors[0]);
+        var colorsName0 = b.results[0].info.image_colors[0].closest_palette_color;
+
+        console.log("Primary Closest Color Name: ", colorsName0);
+
+        scraper(colorsName0);
+
+    }).auth(imaggaapiKey, imaggaapiSecret, true);
+};
+
+var PORT = process.env.PORT || 8080;
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
+
+    module.exports = function(app) {
+        app.get("*", function(req, res) {
+            res.sendFile(path.join(__dirname, "/../public/home.html"));
+       
+        });
+    };
+
 });
+
+// file uploader to Imagga API  https://docs.imagga.com/#content
+// var fs = require('fs'),
+//     request = require("request"),
+//     filePath = "/path/to/image.jpg",
+//     formData = {
+//         image: fs.createReadStream(filePath)
+//     };
+
+// request.post({ url: 'https://api.imagga.com/v1/content', formData: formData },
+//     function (error, response, body) {
+//         var b = JSON.parse(body);
+//         var colorsName0 = b.results[0].info.image_colors[0].closest_palette_color;
+//         var colorsHTML0 = b.results[0].info.image_colors[0].closest_palette_color_html_code;
+//         var colorsPercent0 = b.results[0].info.image_colors[0].percent;
+//         var colorsName1 = b.results[0].info.image_colors[1].closest_palette_color;
+//         var colorsHTML1 = b.results[0].info.image_colors[1].closest_palette_color_html_code;
+//         var colorsPercent1 = b.results[0].info.image_colors[1].percent;
+//         console.log("Primary Closest Color Name: ", colorsName0);
+//         console.log("Primary HTML Color Code: ", colorsHTML0);
+//         console.log("Primary Color Percentage: ", colorsPercent0, "%");
+//         console.log("Secondary Closest Color Name: ", colorsName1);
+//         console.log("Secondary HTML Color Code: ", colorsHTML1);
+//         console.log("Secondary Color Percentage: ", colorsPercent1, "%");
+//     }).auth(imaggaapiKey, imaggaapiSecret, true);
